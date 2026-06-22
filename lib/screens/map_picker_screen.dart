a21dbeeeb90505e92dec991c7a2f0ae8cabba3fa
Carlos Parra <carlos@carlosparra.com.mx>
2026-06-22 15:33:07 -0700

    Fix black map tiles on web: use CancellableNetworkTileProvider

--- lib/screens/map_picker_screen.dart
+++ lib/screens/map_picker_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
@@ -74,6 +75,8 @@ class _MapPickerScreenState extends State<MapPickerScreen> {
               TileLayer(
                 urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                 userAgentPackageName: 'com.carlosparra.dogreidapp',
+                // Proveedor recomendado para WEB (evita tiles en negro en CanvasKit).
+                tileProvider: CancellableNetworkTileProvider(),
               ),
               if (_selected != null)
                 MarkerLayer(markers: [
